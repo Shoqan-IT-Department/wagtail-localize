8ba72a604315fec322064056db05f3d8e40cb9bf
Azadron228 <azamat.ibrayv@gmail.com>
2025-11-13 13:57:30 +0500

    fix

--- wagtail_localize/segments/ingest.py
+++ wagtail_localize/segments/ingest.py
@@ -98,15 +98,6 @@ def handle_related_object(related_model, src_locale, tgt_locale, segments):
     except IndexError:
         pass
 
-    segment = segments[0]
-
-    if isinstance(segment, OverridableSegmentValue):
-        return related_model.objects.get(pk=segment.data)
-
-    else:
-        # Assume it's a RelatedObjectValue
-        return segment.get_instance(tgt_locale)
-
 
 class StreamFieldSegmentsWriter:
     """
